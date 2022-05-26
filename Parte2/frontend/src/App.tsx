9d1d4e590144548fdb34ae062307d4176d1ff9ec
Vitor Junior <71563179+vitia-fritelle@users.noreply.github.com>
2022-05-26 19:34:30 -0300

    frontend /Parte2

---
+++ Parte2/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className="App">
+      Usar o Multer
+      <form action="" method="post">
+        <input type="file" />
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
